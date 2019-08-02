2af962645b13d967827c323c5fb296ed33352aa4
Nikhil Gaur <nikhil.gaur@ranosys.com>
2019-08-02 15:49:02 +0530

    changes to make template test work in offline mode

--- src/components/Layout/Bootstrap.js
+++ src/components/Layout/Bootstrap.js
@@ -1,26 +1,34 @@
 export const BootstrapCss = () => (
+  <>
   <link
     rel="stylesheet"
-    href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    href="http://localhost/static/bootstrap.min.css"
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
     crossOrigin="anonymous"
   />
+  <link
+    rel="stylesheet"
+    href="http://localhost/static/all.css"
+    integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
+    crossorigin="anonymous"
+  />
+  </>
 );
 
 export const BootstrapJs = () => (
   <>
     <script
-      src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+      src="http://localhost/static/jquery-3.3.1.slim.min.js"
       integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
       crossOrigin="anonymous"
     />
     <script
-      src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+      src="http://localhost/static/popper.min.js"
       integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
       crossOrigin="anonymous"
     />
     <script
-      src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+      src="http://localhost/static/bootstrap.min.js"
       integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
       crossOrigin="anonymous"
     />
